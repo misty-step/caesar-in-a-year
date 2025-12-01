194410154c32d784441a9b796b1555837a6248a1
phrazzld <phrazzld@pm.me>
2025-12-01 11:03:17 -0600

    fix: mark LatinText as client component

--- components/UI/LatinText.tsx
+++ components/UI/LatinText.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 
 interface LatinTextProps {
@@ -109,4 +111,3 @@ export const LatinText: React.FC<LatinTextProps> = ({
     </Tag>
   );
 };
-
