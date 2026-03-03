537a78aa89ff42bd5f70e50ad9a3a620d2321f33
phrazzld <phrazzld@pm.me>
2026-03-03 08:06:47 -0600

    fix: unblock favicon PR review blockers

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,12 @@
-import { Sword } from '@phosphor-icons/react'
+'use client';
+
+import { Sword } from '@phosphor-icons/react';
 
 interface LogoProps {
-  size?: number
-  className?: string
+  size?: number;
+  className?: string;
 }
 
 export function Logo({ size = 24, className }: LogoProps) {
-  return <Sword size={size} weight="regular" className={className} />
+  return <Sword size={size} weight='regular' className={className} />;
 }
